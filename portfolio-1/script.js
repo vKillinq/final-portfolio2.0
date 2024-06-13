6d4c811802fca9769e65fa8215ef11322ef74de2
vKillinq <162730016+vKillinq@users.noreply.github.com>
2024-06-13 15:28:05 -0400

    Add files via upload

---
+++ portfolio-1/script.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+function bigImg(x) {
+  x.style.height = "600px";
+  x.style.width = "auto";
+}
+
+function normalImg(x) {
+  x.style.height = "450px";
+  x.style.width = "auto";
+}
+function bigImg2(x) {
+  x.style.height = "600px";
+  x.style.width = "auto";
+}
+
+function normalImg2(x) {
+  x.style.height = "450px";
+  x.style.width = "auto";
+}
